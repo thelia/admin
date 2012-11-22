d0794a20271f65a83ffa8980ca558f6cba30a00b
Manuel Raynaud <raynaud.manu@gmail.com>
2012-11-22 16:01:40 +0100

    ajout de la page pour avertir de renommer le répertoire admin

---
+++ changerep.php
@@ -0,0 +1,46 @@
+<?php
+    require_once __DIR__ . "/../fonctions/error_reporting.php";
+    require_once __DIR__ . "/autoload.php";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+            <link rel="shortcut icon" href="favicon.ico" />
+    
+            <meta charset="utf-8">
+            <title>Thelia - Back Office</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="description" content="">
+            <meta name="author" content="">
+        <!-- Le styles -->
+            <link href="css/bootstrap.css" rel="stylesheet">
+            <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+            <link href="http://www.steawlab.com/thomas/thelia/styles.css" rel="stylesheet">
+    </head>
+<body>
+
+
+    <div class="bg-image">
+<div id="wrapper" class="container"> <!--div id="subwrapper"> -->
+    <div class="row-fluid">
+        <div class="span12">
+            <h3>Veuillez renommer votre répertoire admin avant de vous connecter ...</h3>
+        </div>
+    </div>
+</div><!-- fin div id=wrapper -->
+<hr>
+      <footer class="modal-footer">
+				<div class="container">
+					<p>&copy; Thelia 2012 
+					- <a href="http://www.openstudio.fr/" target="_blank">&Eacute;dit&eacute; par OpenStudio</a>
+					- <a href="http://forum.thelia.net/" target="_blank">Forum Thelia</a>
+					- <a href="http://contrib.thelia.net/" target="_blank">Contributions Thelia</a>
+                                        <span class="pull-right">interface par <a target="_blank" href="http://www.steaw-webdesign.com/">Steaw-Webdesign</a></span>
+					</p>
+                                        
+				</div>
+      </footer>
+</div> <!-- fin div class=bg-image -->
+</body>
+</html>
