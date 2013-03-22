177427adb94bbffe32cdd63b6a46415b5a5e11c6
Manuel Raynaud <raynaud.manu@gmail.com>
2013-03-22 14:40:38 +0100

    correction affichage des contenus associés

--- liste/contenu_associe.php
+++ liste/contenu_associe.php
@@ -4,8 +4,8 @@ function lister_contenuassoc($type, $objet) {
     foreach(AssociatedContentAdmin::getInstance()->getList($type, $objet) as $content){
     ?>
         <tr class="content_liste">
-            <td><?php echo $content["folder"]; ?></td>
-            <td><?php echo $content["content"]; ?></td>
+            <td><?php echo $content["folder_titre"]; ?></td>
+            <td><?php echo $content["content_titre"]; ?></td>
             <td><a href="#" class="content-delete" data-content="<?php echo $content["id"]; ?>"><i class="icon-trash"></i></a></td>
         </tr>
     <?php
